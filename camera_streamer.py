de762adbfaeab87cd897b21067285325795d4f4b
Alan Broun <abroun@alanbroun.net>
2014-04-17 18:18:40 +0100

    Moved CameraStreamer to use raspberry_pi_camera_streamer

--- camera_streamer.py
+++ camera_streamer.py
@@ -41,15 +41,12 @@ class CameraStreamer:
        Therefore, startStreaming must be called periodically. If startStreaming
        is not called before a timeout period expires then the streaming will stop"""
 
-    IMAGE_PATH = "/tmp/stream"
-    IMAGE_NAME = "pic.jpg"
     DEFAULT_TIMEOUT = 4.0
     
     #-----------------------------------------------------------------------------------------------
     def __init__( self, timeout=DEFAULT_TIMEOUT ):
             
-        self.cameraProcess = None
-        self.mjpgStreamerProcess = None
+        self.cameraStreamerProcess = None
         self.streamingStartTime = 0
         self.streamingTimeout = timeout
 
@@ -61,28 +58,12 @@ class CameraStreamer:
     #-----------------------------------------------------------------------------------------------
     def startStreaming( self ):
         
-        fullImagePath = self.IMAGE_PATH + "/" + self.IMAGE_NAME
-        
-        # Create streaming path if needed
-        if not os.path.exists( self.IMAGE_PATH ):
+        # Start raspberry_pi_camera_streamer if needed
+        if self.cameraStreamerProcess == None or self.cameraStreamerProcess.poll() != None:
             
-            os.makedirs( self.IMAGE_PATH )
+            self.cameraStreamerProcess = subprocess.Popen( 
+                [ "raspberry_pi_camera_streamer" ] )
         
-        # Start raspistill if needed
-        if self.cameraProcess == None or self.cameraProcess.poll() != None:
-            
-            self.cameraProcess = subprocess.Popen( [ "raspistill",
-                "--nopreview", "-w", "640", "-h", "480", "-q", "5",
-                "-o", fullImagePath, "-tl", "100", "-t", "9999999", "-th", "0:0:0" ] )
-        
-        # Start mjpg_streamer if needed
-        if self.mjpgStreamerProcess == None or self.mjpgStreamerProcess.poll() != None:
-            
-            self.mjpgStreamerProcess = subprocess.Popen( [ 
-                "/usr/local/bin/mjpg_streamer",
-                "-i", "/usr/local/lib/input_file.so -f {0} -n {1}".format( self.IMAGE_PATH, self.IMAGE_NAME ), 
-                "-o", "/usr/local/lib/output_http.so -w /usr/local/www" ] )
-
         self.streamingStartTime = time.time()         
                 
     #-----------------------------------------------------------------------------------------------
@@ -91,19 +72,9 @@ class CameraStreamer:
         if time.time() - self.streamingStartTime > self.streamingTimeout:
             
             self.stopStreaming()
-            
-        #else:
-            #if self.cameraProcess != None:
-                #print "Camera", self.cameraProcess.poll()
-            
-            #if self.mjpgStreamerProcess != None:
-                #print "mjpg", self.mjpgStreamerProcess.poll()
                 
     #-----------------------------------------------------------------------------------------------
     def stopStreaming( self ):
 
-        if self.cameraProcess != None:
-            self.cameraProcess.terminate()
-            
-        if self.mjpgStreamerProcess != None:
-            self.mjpgStreamerProcess.terminate()
+        if self.cameraStreamerProcess != None:
+            self.cameraStreamerProcess.terminate()
